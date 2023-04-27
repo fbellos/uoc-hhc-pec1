ecbcad48b42d727d03a9719d78e9327c6a8a3dcc
felipe bello santos <fbellos@uoc.edu>
2023-04-27 03:04:55 +0200

    funcionalidad js

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,34 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+function filtrar() {
+
+    let isCheckedMonumentos = document.getElementById("monumentos").checked;
+    let isCheckedMuseos = document.getElementById("museos").checked;
+    let isCheckedIglesias = document.getElementById("iglesias").checked;
+
+    let seccionMonumentos = document.getElementsByClassName("monumentos")[0];
+    if(!isCheckedMonumentos) {
+        seccionMonumentos.classList.add("d-none");
+    } else {
+        if (seccionMonumentos.classList.contains("d-none")) {
+            seccionMonumentos.classList.remove("d-none");
+        }
+    }
+
+    let seccionMuseos = document.getElementsByClassName("museos")[0];
+    if(!isCheckedMuseos) {
+        seccionMuseos.classList.add("d-none");
+    } else {
+        if (seccionMuseos.classList.contains("d-none")) {
+            seccionMuseos.classList.remove("d-none");
+        }
+    }
+
+    let seccionIglesias = document.getElementsByClassName("iglesias")[0];
+    if(!isCheckedIglesias) {
+        seccionIglesias.classList.add("d-none");
+    } else {
+        if (seccionIglesias.classList.contains("d-none")) {
+            seccionIglesias.classList.remove("d-none");
+        }
+    }
+}
+
